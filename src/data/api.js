fe157443c0ea8bf32439c98ce7d7ab31a09d6250
Muhammad Hanif <hanifeij@yahoo.co.id>
2019-05-22 20:36:47 +0700

    last materi

--- src/data/api.js
+++ src/data/api.js
@@ -1,5 +1,5 @@
-export function getNews() {
-  let url = "https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=b8e624aaad324e60be0b7e72f926ed4c"
+export function getNews(page = 1) {
+  let url = `https://newsapi.org/v2/top-headlines?country=id&category=business&apiKey=b8e624aaad324e60be0b7e72f926ed4c&page=${page}`;
   return fetch(url)
     .then(response => {
       return response.json();
--- src/pages/App.js
+++ src/pages/App.js
@@ -8,18 +8,41 @@ import { getNews } from '../data/api';
 
 class App extends React.Component {
   state = {
+    page: 1,
     dataArticle: [],
     loading: true,
+    error: false,
   };
 
   componentDidMount() {
-    getNews().then(data => {
-      console.log(data);
-      this.setState({
-        dataArticle: data.articles,
-        loading: false,
+    this.fetchNews();
+  }
+
+  fetchNews(page = 1) {
+    getNews(page)
+      .then(data => {
+        console.log(data);
+        const { dataArticle } = this.state;
+        const nextData = dataArticle.concat(data.articles);
+        this.setState({
+          dataArticle: nextData,
+          loading: false,
+        })
       })
-    })
+      .catch(error => {
+        console.log(error);
+        this.setState({
+          loading: false,
+          error: true,
+        })
+      });
+  }
+
+  fetchNextPage() {
+    const { page } = this.state;
+    const nextPage = page + 1;
+    this.fetchNews(nextPage);
+    this.setState({ page: nextPage });
   }
 
   renderLoading() {
@@ -28,16 +51,37 @@ class App extends React.Component {
     </h3>
   }
 
+  renderEmpty() {
+    return <h5 className="mt-5 text-center text-muted">
+      Tidak ada berita.
+    </h5>
+  }
+
+  renderError() {
+    return (
+      <div>
+        <Header title="ERROR !!" />
+        <h5 className="mt-5 text-center text-danger">
+          Terdapat Error Saat Meload Berita.
+        </h5>
+      </div>
+    )
+  }
+
   render() {
-    const { dataArticle, loading } = this.state;
+    const { dataArticle, loading, error } = this.state;
+    const isEmpty = !loading && dataArticle.length === 0;
+    if (error) return this.renderError();
     return (
       <div>
         <Header title="Beritaku" />
         {loading && this.renderLoading()}
+        {isEmpty && this.renderEmpty()}
         {!loading && (
           <div className="news-container px-5">
             {dataArticle.map((article) => {
               return <Card
+                url={article.url}
                 image={article.urlToImage}
                 title={article.title}
                 author={article.author}
@@ -45,6 +89,15 @@ class App extends React.Component {
                 published={article.publishedAt}
               />
             })}
+          </div>
+        )}
+        {!isEmpty && (
+          <div className="mb-5 mt-5 text-center">
+            <button
+              onClick={() => this.fetchNextPage()}
+              className="btn btn-outline-primary btn-lg">
+              Load More
+            </button>
           </div>
         )}
       </div>
